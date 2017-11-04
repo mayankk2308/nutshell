02f5b416685154e2be9b37f8b76b17b6a7eb007c
Mayank Kumar <mayankk2308@gmail.com>
2017-11-04 04:03:42 -0400

    Remove debug msg in 'organize.sh'

--- scripts/organize.sh
+++ scripts/organize.sh
@@ -9,7 +9,6 @@ folder_location=`./scripts/find.sh "$folder"`
 if [[ -d "$folder_location" ]]
 then
     final_dest="$folder_location"/"$file_type"
-    echo $final_dest
     mkdir "$final_dest"
     mv "$folder_location"/*."$file_type" "$final_dest"
 fi
