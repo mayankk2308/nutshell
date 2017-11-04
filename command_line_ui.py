308a1c5fddbfe6164396dc1fe5a37c02a9a74483
Vedant Puri <vedantpuri13@gmail.com>
2017-11-04 19:42:05 -0400

    Prelim UI implementation for find and open

--- command_line_ui.py
+++ command_line_ui.py
@@ -1,24 +1,53 @@
 from appJar import gui
 import nlu
 
-def handleFind(response):
-    if response == 0:
-        app.scr
-        app.infoBox("Error", "File/Folder not found")
+
+def reset_box():
+    app.removeAllWidgets()
+    app.addLabel("Title", "Natural Language Unix")
+    app.addLabelEntry("Enter your Command: ")
+    app.addButtons(["Submit"], requestHandler)
+
+
+def radiochoice(rb):
+    user = app.getRadioButton("option")
+    reset_box()
+    app.infoBox("Response", nlu.opencmd(user))
+
+def radioHandler(options):
+    for item in options:
+        app.addRadioButton("option", item)
+    app.addButtons(["Choose"], radiochoice)
+
+def findHandler(output):
+    output_destinations = ""
+    line_number = 1
+    for line in output:
+        output_destinations += str(line_number) + ") " + line + "\n\n"
+        line_number += 1
+    app.infoBox("Locations", output_destinations)
+
+def openHandler(output):
+
+    if len(output) == 1:
+        app.infoBox("Response", nlu.opencmd(output[0]))
     else:
-        ctr = 1
-        output = ""
-        for option in response:
-            output += str(ctr) +")" + " " + option + "\n\n"
-            ctr += 1
-        app.infoBox("File Path List",output)
+        radioHandler(output)
+
 
 def requestHandler(button):
     if button == "Submit":
-        request = app.getEntry("Enter your Command: ")
-        command,response = nlu.main(request)
-        if command == "find":
-            handleFind(response)
+        user_command = app.getEntry("Enter your Command: ");
+        command_name, output = nlu.main(user_command)
+        if output is None:
+            app.infoBox("Error", command_name)
+
+        if command_name == "find":
+            findHandler(output)
+
+        elif command_name == "open":
+            openHandler(output)
+
 
 app = gui()
 app.addLabel("Title", "Natural Language Unix")
