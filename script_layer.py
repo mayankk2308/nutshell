14fe89788234498e052d766e5439090e97d56940
Mayank Kumar <mayankk2308@gmail.com>
2017-11-05 17:34:33 -0500

    Add command execution + response processing

--- script_layer.py
+++ script_layer.py
@@ -9,25 +9,30 @@ class script_manager(object):
         command += " "
         current_arg = ""
         arg_count = -1
-        script, args = "", []
+        args = []
         for character in command:
             if character is " " and quotes % 2 == 0:
                 arg_count += 1
-                if arg_count == 0:
-                    script = current_arg
-                else:
-                    args.append(current_arg)
+                args.append(current_arg)
                 current_arg = ""
-            elif character is "\"":
+            elif character is "'":
                 quotes += 1
             else:
                 current_arg += character
 
-        return (script, args)
+        return args
 
-    # primary command execution handler
+    # primary command execution handler which returns a response (output or error)
     def execute(self, command, expected_args):
-        script, args = self.parse_command(command, expected_args)
-        if len(args) is not expected_args: return
-        print(script)
-        print(args)
+        args = self.parse_command(command, expected_args)
+        if len(args) - 1 is not expected_args: return
+        response = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return self.process_response(response)
+
+    # process standard I/O
+    def process_response(self, response):
+        stdout, stderr = response.communicate()
+        return stderr.rstrip().decode("utf-8") if stderr else stdout.rstrip().decode("utf-8")
+
+manager = script_manager()
+print(manager.execute("scripts/find.sh purge-nvda.sh", 1))
