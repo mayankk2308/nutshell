38c770e162fcaed06984baae9e76dd7d3e056604
Vedant Puri <vedantpuri13@gmail.com>
2017-11-04 22:11:16 -0400

    Alternate UI implementation for multiple src and dest

--- command_line_ui.py
+++ command_line_ui.py
@@ -1,7 +1,8 @@
 from appJar import gui
 import nlu
 
-
+command = None
+user = None
 def reset_box():
     app.removeAllWidgets()
     app.addLabel("Title", "Natural Language Unix")
@@ -10,14 +11,36 @@ def reset_box():
 
 
 def radiochoice(rb):
-    user = app.getRadioButton("option")
-    reset_box()
-    app.infoBox("Response", nlu.opencmd(user))
+    if command == "open":
+        user = app.getRadioButton("option")
+        reset_box()
+        app.infoBox("Response", nlu.opencmd(user))
+
+    else:
+        global user
+        user = app.getRadioButton("option")
+
+def radiochoice1(rb):
+        global user
+        user2 = app.getRadioButton("option1")
+        print("The command is:",command,user,user2)
 
 def radioHandler(options):
-    for item in options:
-        app.addRadioButton("option", item)
-    app.addButtons(["Choose"], radiochoice)
+    if command == "open":
+        for item in options:
+            app.addRadioButton("option", item)
+        app.addButtons(["Choose"], radiochoice)
+
+    else:
+        for item in options[0]:
+            app.addRadioButton("option", item)
+        app.addButtons(["Choose"], radiochoice)
+
+        for item in options[1]:
+            app.addRadioButton("option1", item)
+        app.addButtons(["ChooseSecond"], radiochoice1)
+
+
 
 def findHandler(output):
     output_destinations = ""
@@ -32,8 +55,12 @@ def openHandler(output):
     if len(output) == 1:
         app.infoBox("Response", nlu.opencmd(output[0]))
     else:
+        global command
+        command = "open"
         radioHandler(output)
 
+def copyHandler(output):
+    radioHandler(output)
 
 def requestHandler(button):
     if button == "Submit":
@@ -42,11 +69,16 @@ def requestHandler(button):
         if output is None:
             app.infoBox("Error", command_name)
 
-        if command_name == "find":
+        elif command_name == "find":
             findHandler(output)
 
         elif command_name == "open":
             openHandler(output)
+
+        else:
+            global command
+            command = command_name
+            copyHandler(output)
 
 
 app = gui()
