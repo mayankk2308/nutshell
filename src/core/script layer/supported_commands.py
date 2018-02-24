f479b4eab79219c13e65c40bd6660e2c2e596204
Mayank Kumar <mayankk2308@gmail.com>
2018-02-24 16:16:05 -0500

    Add completion handler + auto-expected args

--- src/core/script layer/supported_commands.py
+++ src/core/script layer/supported_commands.py
@@ -1,4 +1,5 @@
 from directory_service import UNIX_SCRIPTS_DIR, WIN_SCRIPTS_DIR
+
 AVAILABLE_COMMANDS = {
 "open" : UNIX_SCRIPTS_DIR + "open.sh",
 "find" : UNIX_SCRIPTS_DIR + "find.sh",
@@ -6,4 +7,13 @@ AVAILABLE_COMMANDS = {
 "move" : UNIX_SCRIPTS_DIR + "move.sh",
 "rename" : UNIX_SCRIPTS_DIR + "move.sh",
 "organize": UNIX_SCRIPTS_DIR + "organize.sh"
+}
+
+EXPECTED_ARGS = {
+"open": 1,
+"find": 1,
+"copy": 2,
+"move": 2,
+"rename": 2,
+"organize": 2
 }
