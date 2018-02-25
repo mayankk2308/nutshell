a4198b6f67195ca87c5a0975412d2e01d6b91ef5
Mayank Kumar <mayankk2308@gmail.com>
2018-02-24 21:08:46 -0500

    Add management directory path reference to directory service

--- src/core/unix layer/directory_service.py
+++ src/core/unix layer/directory_service.py
@@ -6,6 +6,7 @@ _script_layer = _abs_path[: _abs_path.rfind("/")]
 _core = _script_layer[: _script_layer.rfind("/")]
 _src = _core[: _core.rfind("/")]
 BASE_DIR = _src[: _src.rfind("/") + 1]
+MANAGEMENT_DIR = _src + "/management"
 UNIX_SCRIPTS_DIR = BASE_DIR + "scripts/unix_scripts/"
 WIN_SCRIPTS_DIR = BASE_DIR + "scripts/win_scripts/"
 LOG_DIR = BASE_DIR + "logs/"
