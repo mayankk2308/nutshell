8f8c7a8a6b249d97c69402c1d19ccd06375d0300
Mayank Kumar <mayankk2308@gmail.com>
2018-02-26 01:06:50 -0500

    Add skeleton Xcode application

---
+++ src/apps/macos/NLU/NLU/CoreDataStack.swift
@@ -0,0 +1,49 @@
+//
+//  CoreDataStack.swift
+//  NLU
+//
+//  Created by Mayank Kumar on 2/26/18.
+//  Copyright © 2018 Mayank Kumar. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    private static var instance: CoreDataStack!
+    
+    static func sharedInstance() -> CoreDataStack {
+        if instance == nil {
+            instance = CoreDataStack()
+        }
+        return instance
+    }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: "NLU")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error)")
+            }
+        })
+        return container
+    }()
+}
---
+++ src/apps/macos/NLU/NLU/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NLU
+//
+//  Created by Mayank Kumar on 2/26/18.
+//  Copyright © 2018 Mayank Kumar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
